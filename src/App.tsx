ce57ad9ac0d47201c75fbffc9f2800187567dd9d
SirLucasLima <doislcompany@gmail.com>
2023-01-18 03:36:22 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Habit } from "./components/Habit";
+import "./styles/global.css"
+
+export default function App() {
+  return (
+    <div>
+      <Habit completed={3}/>
+    </div>    
+  )
+}
